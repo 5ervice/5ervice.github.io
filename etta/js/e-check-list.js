a28fcdae49d789f019cbd55f4e2ab6622b1e15da
Lorcan <lorcanblake@hotmail.com>
2019-03-18 14:58:39 +0000

    P/E: Onboarding, Account, Following component updates

--- etta/js/e-check-list.js
+++ etta/js/e-check-list.js
@@ -1,6 +1,6 @@
 var check = document.getElementById("check");
 
 function select(elem) {
-  elem.classList.add("b");
-  check.classList.add("dib");
+  elem.classList.toggle("b");
+  check.classList.toggle("dib");
 }
---
+++ etta/js/e-follow.js
@@ -0,0 +1,7 @@
+var following = document.getElementById("following");
+var follow = document.getElementById("follow");
+
+function select() {
+  follow.classList.toggle("dn");
+  following.classList.toggle("db");
+}
