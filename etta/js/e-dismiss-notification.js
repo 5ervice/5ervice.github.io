6833900174a934330108918514355f6f2018bcc5
Lorcan <lorcanblake@hotmail.com>
2019-03-25 20:30:47 +0000

    Etta: added FAQ/Modal components

--- etta/js/e-dismiss-notification.js
+++ etta/js/e-dismiss-notification.js
@@ -1,7 +1,13 @@
 var notif = document.getElementById("notif");
+// var view = document.getElementById("main");
 
 function dismiss(){
+  // if(view.classList.contains("blur")){
+  //   view.classList.remove("blur");
+  // }
+  
   notif.classList.add("o-40");
+  
   setTimeout(function () {
     notif.remove();
 }, 600);
